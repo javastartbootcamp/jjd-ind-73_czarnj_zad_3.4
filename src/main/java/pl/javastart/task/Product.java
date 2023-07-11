7588db2a32a00651ad0ce4e67c69aa62602303b1
Julka Czarnasiak <czarna13500@gmail.com>
2023-07-11 19:17:47 +0200

    Add changes regarding review

--- src/main/java/pl/javastart/task/Product.java
+++ src/main/java/pl/javastart/task/Product.java
@@ -14,6 +14,6 @@ public class Product {
     }
 
     public String getInfo() {
-        return "Product: "+ name + "\nPrice: " + price + "zł\nDescription: " + description;
+        return "Product: " + name + "\nPrice: " + price + "zł\nDescription: " + description;
     }
 }
